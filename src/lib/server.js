f5d849b3e9d7db6376a612309140fa84bb9c1fed
Edu-Jeps <eduardojp205@gmail.com>
2026-06-13 14:54:24 -0300

    feat: criação do rateLimit

--- src/lib/server.js
+++ src/lib/server.js
@@ -3,12 +3,22 @@ import taskRoutes from '../client/routes/task.js'
 import authRoutes from '../client/routes/auth.js'
 import 'dotenv/config'
 import { errorHandler } from '../middleware/errorHandler.js'
+import rateLimit from 'express-rate-limit'
 
 const app = express()
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 10, // Limite de 10 requisições por IP
+    message: { error: 'Muitas requisições, por favor tente novamente mais tarde.' }
+})
+
+app.use(limiter)
 app.use(express.json())
 app.use('/tasks', taskRoutes)
 app.use('/auth', authRoutes)
 app.use(errorHandler)
+
 
 app.listen(3000, () => {
     console.log('Servidor rodando')
